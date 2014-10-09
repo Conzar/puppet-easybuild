d85df0ca3f658a8c01e61519301a00f613883e96
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-09 16:28:18 +0200

    script terminé et fonctionnel (pas utile pour le moment, mais pratique si je dois installer rapidement un EasyBuild temporaire car tout est compris dans le dossier d'installation)

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,18 +4,24 @@ class easybuild {
 
         exec { 'install-eb':
                 user    => 'swuser',
-                command => "bash -c '. /usr/share/?odules/init/bash && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
+                command => "bash -c 'cd /tmp && wget https://raw.githubusercontent.com/sylmarien/easybuild-install/feature/autoinstall/install-EasyBuild-develop.sh && mkdir /opt/apps/EasyBuild && sh install-EasyBuild-develop.sh /opt/apps/EasyBuild && rm install-EasyBuild-develop.sh'",
 		creates => "/opt/apps/EasyBuild",
-		umask   => '0000',
-                require => [ File [ '/opt' ],
+		umask   => '022',
+                require => [ File [ '/opt/apps' ],
                         User [ 'swuser' ],
                         Package [ 'environment-modules' ],
                         ]
         }
 
+	file { '/opt/apps':
+		ensure  => directory,
+		owner   => 'swuser',
+		require => File [ '/opt' ],
+	}
+
         file { '/opt':
                 ensure => directory,
-                mode => '0777',
+                owner => 'swuser',
         }
 
         user { 'swuser':
@@ -44,14 +50,15 @@ class easybuild {
 
         exec { 'GitInit':
 		user => 'swuser',
-                command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild.git .'",
+                command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git .'",
                 creates => "/tmp/eb_config/.git",
                 require => File [ '/tmp/eb_config' ],
         }
 
         file { '/tmp/eb_config':
                 ensure => directory,
-                mode => '0777',
+		owner => 'swuser',
+		require => User [ 'swuser' ],
         }
 }
 
