dbfdca38c6165f52ab9a7e8b45905cdc4b976d7a
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-09 12:12:03 +0200

    modification du manifest pour fonctionner sur CentOS et Debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@ class easybuild {
 
         exec { 'install-eb':
                 user    => 'swuser',
-                command => 'cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py',
+                command => "bash -c '. /usr/share/?odules/init/bash && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
 		creates => "/opt/apps/EasyBuild",
 		umask   => '0000',
                 require => [ File [ '/opt' ],
@@ -24,12 +24,7 @@ class easybuild {
 
         package { 'environment-modules':
                 ensure => latest,
-                before => Exec [ 'moduleInit' ],
-        }
-
-        exec { 'moduleInit':
-                user => 'swuser',
-                command => "bash -c '. /usr/share/Modules/init/bash'",
+		require => User [ 'swuser' ],
         }
 
         #configure eb env
@@ -37,17 +32,19 @@ class easybuild {
                 ensure => file,
                 owner => 'swuser',
                 source => "/tmp/eb_config/easybuild.sh",
-                require => Exec [ 'Git' ],
+                require => [ Exec [ 'Git' ], User [ 'swuser' ] ],
         }
 
         exec { 'Git':
+		user => 'swuser',
                 # On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
-                command => 'cd /tmp/eb_config && git pull origin master',
+                command => "bash -c 'cd /tmp/eb_config && git pull origin master'",
                 require => Exec [ 'GitInit' ],
         }
 
         exec { 'GitInit':
-                command => 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild.git .',
+		user => 'swuser',
+                command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild.git .'",
                 creates => "/tmp/eb_config/.git",
                 require => File [ '/tmp/eb_config' ],
         }
