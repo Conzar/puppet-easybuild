11bb335d2ce7bbefb08c6812053b47a091bceeb9
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-10 17:04:21 +0200

    en cours de modification pour avoir un seul manifest pour Debian et CentOS

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,43 @@
 class easybuild {
 
         Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/share/lmod/lmod/libexec/" ] }
+	
+	$branch = $::operatingsystem ? {
+		'CentOS' => 'feature/Lmod',
+		'Debian' => 'master',
+	}
 
-        exec { 'install-eb':
-		user    => 'swuser',
-		command => "bash -c 'source /usr/share/lmod/lmod/init/profile && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
-		creates => "/opt/apps/EasyBuild",
-		umask   => '022',
-		require => [ File [ '/opt' ], User [ 'swuser' ], Package [ 'Lmod' ] ],
-        }
+	case $::operatingsystem {
+		'CentOS': {
+		        exec { 'install-eb':
+				user    => 'swuser',
+				command => "bash -c 'source /usr/share/lmod/lmod/init/profile && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
+				creates => "/opt/apps/EasyBuild",
+				umask   => '022',
+				require => [ File [ '/opt' ], User [ 'swuser' ], Package [ 'Lmod' ] ],
+		       	}
+
+			package { 'Lmod':
+				ensure          => 'installed',
+				install_options => '--enablerepo=epel-testing',
+			}
+		}
+
+		'Debian': {
+			exec  { 'install-eb':
+				user    => 'swuser',
+				command => "bash -c 'source /usr/share/lmod/lmod/init/profile && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
+				creates => "/opt/apps/EasyBuild",
+				umask   => '022',
+				require => [ File [ '/opt' ], User [ 'swuser' ], Package [ 'environment-modules' ] ],
+			}
+
+			package { 'environment-modules':
+				ensure          => 'installed',
+				install_options => [ '-t', 'jessie' ],
+			}
+		}
+	}
 
         file { '/opt':
 		ensure  =>  directory,
@@ -22,11 +51,6 @@ class easybuild {
 		managehome => true,
         }
 
-        package { 'Lmod':
-		ensure          => 'installed',
-		install_options => '--enablerepo=epel-testing',
-        }
-
         #configure eb env
         file { '/etc/profile.d/easybuild.sh':
 		ensure  => file,
@@ -38,13 +62,13 @@ class easybuild {
 	# On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
         exec { 'Git':
 		user    => 'swuser',
-		command => "bash -c 'cd /tmp/eb_config && git checkout feature/Lmod && git pull origin feature/Lmod'",
+		command => "bash -c 'cd /tmp/eb_config && git checkout ${branch} && git pull origin ${branch}'",
 		require => Exec [ 'GitInit' ],
 	}
 
         exec { 'GitInit':
 		user    => 'swuser',
-		command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git . && git checkout feature/Lmod'",
+		command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git . && git checkout ${branch}'",
 		creates => "/tmp/eb_config/.git",
 		require => File [ '/tmp/eb_config' ],
 	}
