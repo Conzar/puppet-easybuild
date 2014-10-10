c310848b82971791b0e342278bd396a6c8dccb74
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-10 13:56:30 +0200

    correction mise en forme du code pour lisibilité

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,57 +7,51 @@ class easybuild {
                 command => "bash -c '. /usr/share/?odules/init/bash && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
 		creates => "/opt/apps/EasyBuild",
 		umask   => '022',
-                require => [ File [ '/opt' ],
-                        User [ 'swuser' ],
-                        Package [ 'Lmod' ],
-                        ]
-        }
+                require => [ File [ '/opt' ], User [ 'swuser' ], Package [ 'environment-modules' ] ],
+	}
 
         file { '/opt':
-                ensure => directory,
-		owner => 'swuser',
+		ensure  => directory,
+		owner   => 'swuser',
 		require => User [ 'swuser' ],
-        }
+	}
 
         user { 'swuser':
                 ensure => 'present',
         }
 
-        package { 'Lmod':
-                ensure => 'installed',
-		require => Package [ 'lua5.2' ],
+        package { 'environment-modules':
+		ensure          => 'installed',
+		install_options => 
+		require         => Package [ 'lua5.2' ],
         }
-
-	package { 'lua5.2':
-		ensure => 'installed',
-	}
 
         #configure eb env
         file { '/etc/profile.d/easybuild.sh':
-                ensure  => file,
-		owner => 'swuser',
-                source  => "/tmp/eb_config/easybuild.sh",
-                require => Exec [ 'Git' ],
-        }
+		ensure  => file,
+		owner   => 'swuser',
+		source  => "/tmp/eb_config/easybuild.sh",
+		require => Exec [ 'Git' ],
+	}
 
+	# On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
         exec { 'Git':
-		user => 'swuser',
-                # On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
-                command => "bash -c 'cd /tmp/eb_config && git pull origin master'",
-                require => Exec [ 'GitInit' ],
-        }
+		user    => 'swuser',
+		command => "bash -c 'cd /tmp/eb_config && git pull origin master'",
+		require => Exec [ 'GitInit' ],
+	}
 
         exec { 'GitInit':
-		user => 'swuser',
-                command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git .'",
-                creates => "/tmp/eb_config/.git",
-                require => File [ '/tmp/eb_config' ],
-        }
+		user    => 'swuser',
+		command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git .'",
+		creates => "/tmp/eb_config/.git",
+		require => File [ '/tmp/eb_config' ],
+	}
 
         file { '/tmp/eb_config':
-                ensure => directory,
-		owner => 'swuser',
+		ensure  => directory,
+		owner   => 'swuser',
 		require => User [ 'swuser' ],
-        }
+	}
 }
 
