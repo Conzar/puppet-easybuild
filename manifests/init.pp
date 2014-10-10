65a923f4bc046a5e459b148b92f7bf05c9ce90bb
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-10-10 12:41:54 +0200

    installation EasyBuild en de basant sur Lmod à la place d'environment-modules. Ne marche pas sur Debian car pas de paquet Lmod sur Debian pour le moment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,63 +1,63 @@
 class easybuild {
 
-        Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+        Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/", "/usr/share/lmod/lmod/libexec/" ] }
 
         exec { 'install-eb':
-                user    => 'swuser',
-                command => "bash -c '. /usr/share/?odules/init/bash && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
+		user    => 'swuser',
+		command => "bash -c 'source /usr/share/lmod/lmod/init/profile && cd /tmp && wget https://raw.github.com/hpcugent/easybuild-framework/develop/easybuild/scripts/bootstrap_eb.py && python bootstrap_eb.py /opt/apps/EasyBuild && rm bootstrap_eb.py'",
 		creates => "/opt/apps/EasyBuild",
 		umask   => '022',
-                require => [ File [ '/opt' ],
-                        User [ 'swuser' ],
-                        Package [ 'Lmod' ],
-                        ]
+		require => [ File [ '/opt' ], User [ 'swuser' ], Package [ 'Lmod' ] ],
         }
 
         file { '/opt':
-                ensure => directory,
-		owner => 'swuser',
+		ensure  =>  directory,
+		owner   => 'swuser',
 		require => User [ 'swuser' ],
         }
 
         user { 'swuser':
-                ensure => 'present',
+		ensure     => 'present',
+		home       => '/home/swuser',
+		managehome => true,
         }
 
         package { 'Lmod':
-                ensure => 'installed',
-		require => Package [ 'lua5.2' ],
+		ensure          => 'installed',
+		install_options => '--enablerepo=epel-testing',
+		require         => Package [ 'lua' ],
         }
 
-	package { 'lua5.2':
+	package { 'lua':
 		ensure => 'installed',
 	}
 
         #configure eb env
         file { '/etc/profile.d/easybuild.sh':
-                ensure  => file,
-		owner => 'swuser',
-                source  => "/tmp/eb_config/easybuild.sh",
-                require => Exec [ 'Git' ],
-        }
+		ensure  => file,
+		owner   => 'swuser',
+		source  => "/tmp/eb_config/easybuild.sh",
+		require => Exec [ 'Git' ],
+	}
 
+	# On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
         exec { 'Git':
-		user => 'swuser',
-                # On pull, on n'a pas peur d'avoir de probleme de merge car a priori les serveurs ne modifient pas localement le fichier
-                command => "bash -c 'cd /tmp/eb_config && git pull origin master'",
-                require => Exec [ 'GitInit' ],
-        }
+		user    => 'swuser',
+		command => "bash -c 'cd /tmp/eb_config && git pull origin master'",
+		require => Exec [ 'GitInit' ],
+	}
 
         exec { 'GitInit':
-		user => 'swuser',
-                command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git .'",
-                creates => "/tmp/eb_config/.git",
-                require => File [ '/tmp/eb_config' ],
-        }
+		user    => 'swuser',
+		command => "bash -c 'cd /tmp/eb_config && git clone https://github.com/sylmarien/easybuild-config.git .'",
+		creates => "/tmp/eb_config/.git",
+		require => File [ '/tmp/eb_config' ],
+	}
 
         file { '/tmp/eb_config':
-                ensure => directory,
-		owner => 'swuser',
+		ensure  => directory,
+		owner   => 'swuser',
 		require => User [ 'swuser' ],
-        }
+	}
 }
 
